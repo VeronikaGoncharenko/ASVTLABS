159c27a8d67ce0f52a77cf9338672a88685387d8
Veronika <veronikagoncharenko2004@gmail.com>
2024-11-26 16:38:50 +0300

    Сделала лабу 3

---
+++ LAB3/DistributedQueue.Common/Interface1.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace DistributedQueue.Common
+{
+    public interface IComputeFactorialJob
+    {
+        Task ComputeFactorialAsync(string name, int number, CancellationToken token);
+    }
+}
---
+++ LAB3/DistributedQueue.Worker/Class1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DistributedQueue.Common
+{
+    public class FactorialJob : IComputeFactorialJob
+    {
+        public async Task ComputeFactorialAsync(string name, int number, CancellationToken token)
+        {
+            if (number < 0)
+            {
+                Console.WriteLine($"{DateTime.Now}: Factorial of negative number {name} is not defined.");
+                return;
+            }
+
+            long result = 1;
+
+            for (int i = 1; i <= number; i++)
+            {
+                if (token.IsCancellationRequested)
+                {
+                    Console.WriteLine($"{DateTime.Now}: Factorial computation for {name} cancelled.");
+                    return;
+                }
+
+                result *= i;
+
+                // Имитация долгих вычислений
+                await Task.Yield(); // Позволяет другим задачам выполняться
+            }
+
+            Console.WriteLine($"Factorial of {number} is {result} (computed by {name}).");
+        }
+    }
+}
